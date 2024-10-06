5d493db488a97c3a638bdd06c91eadd8fb0254e4
김경수 <galtab0403@gmail.com>
2024-10-06 10:35:20 +0900

    Create Room Editor

---
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HelperUtilities
+{
+    public static Camera mainCamera;
+
+
+    public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
+    {
+        if (stringToCheck == "")
+        {
+            Debug.Log(fieldName + " is empty and must contain a value in object " + thisObject.name.ToString());
+            return true;
+        }
+
+        return false;
+    }
+    
+
+    /// <summary>
+    /// Get the mouse world position.
+    /// </summary>
+    public static Vector3 GetMouseWorldPosition()
+    {
+        if (mainCamera == null) mainCamera = Camera.main;
+
+        Vector3 mouseScreenPosition = Input.mousePosition;
+
+        // Clamp mouse position to screen size
+        mouseScreenPosition.x = Mathf.Clamp(mouseScreenPosition.x, 0f, Screen.width);
+        mouseScreenPosition.y = Mathf.Clamp(mouseScreenPosition.y, 0f, Screen.height);
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+
+        worldPosition.z = 0f;
+
+        return worldPosition;
+
+    }
+
+    /// <summary>
+    /// Get the camera viewport lower and upper bounds
+    /// </summary>
+    public static void CameraWorldPositionBounds(out Vector2Int cameraWorldPositionLowerBounds, out Vector2Int cameraWorldPositionUpperBounds, Camera camera)
+    {
+        Vector3 worldPositionViewportBottomLeft = camera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f));
+        Vector3 worldPositionViewportTopRight = camera.ViewportToWorldPoint(new Vector3(1f, 1f, 0f));
+
+        cameraWorldPositionLowerBounds = new Vector2Int((int)worldPositionViewportBottomLeft.x, (int)worldPositionViewportBottomLeft.y);
+        cameraWorldPositionUpperBounds = new Vector2Int((int)worldPositionViewportTopRight.x, (int)worldPositionViewportTopRight.y);
+    }
+
+    /// <summary>
+    /// Get the angle in degrees from a direction vector
+    /// </summary>
+    public static float GetAngleFromVector(Vector3 vector)
+    {
+        float radians = Mathf.Atan2(vector.y, vector.x);
+
+        float degrees = radians * Mathf.Rad2Deg;
+
+        return degrees;
+
+    }
+
+    /// <summary>
+    /// Get the direction vector from an angle in degrees
+    /// </summary>
+    /// <returns></returns>
+    public static Vector3 GetDirectionVectorFromAngle(float angle)
+    {
+        Vector3 directionVector = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle), 0f);
+        return directionVector;
+    }
+
+
+    public static bool ValidateCheckEnumerableValues(RoomNodeTypeListSO thisObject, string fieldName, List<RoomNodeTypeSO> enumerableObjectToCheck)
+    {
+        bool error = false;
+        int count = 0;
+
+        foreach (var item in enumerableObjectToCheck)
+        {
+            if (item == null)
+            {
+                Debug.Log(fieldName + " has null values in object " + thisObject.name.ToString());
+                error = true;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Debug.Log(fieldName + " has no values in object " + thisObject.name.ToString());
+            error = true;
+        }
+
+        return error;
+    }
+}
